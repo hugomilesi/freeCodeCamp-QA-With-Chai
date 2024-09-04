4a51731ee7d757e6ce42b4f785bd12cc9ddd2b1f
Hugo Milesi <hugogmilesi@gmail.com>
2024-09-04 23:48:11 +0000

    Course Completed =D

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,19 +70,19 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.site = "https://36895-freecodecam-boilerplate-3pdgfyp72ym.ws-us116.gitpod.io";
+Browser.site = "https://3000-freecodecam-boilerplate-3pdgfyp72ym.ws-us116.gitpod.io";
 
 const browser = new Browser();
-suiteSetup(function(done) {
-  return browser.visit('/', done);
-});
-
 
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
+  
+  
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -93,15 +93,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", 'Colombo').then(()=>{
+        browser.pressButton("submit",() => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo')
+          browser.assert.elements("span#dates", 1);
+          done();
+        })
+      })
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", 'Vespucci').then(()=>{
+        browser.pressButton("submit",() => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci')
+          browser.assert.elements("span#dates", 1);
+          done();
+        })
+      })
     });
   });
 });
